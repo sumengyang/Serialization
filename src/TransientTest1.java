dc02a2a8a4bbc2c330b5f19bb1ffc0656a06ea90
sumengyang <842298634@qq.com>
2016-09-14 19:16:28 +0800

    序列化对象与transient

---
+++ src/TransientTest1.java
@@ -0,0 +1,81 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description 
+ * 使用transient关键字不序列化某个变量 
+ * 注意读取的时候，读取数据的顺序一定要和存放数据的顺序保持一致
+ */
+public class TransientTest1 {
+	public static void main(String[] args) {
+
+		User2 user = new User2();
+		user.setUsername("Alexia");
+		user.setPasswd("123456");
+
+		System.out.println("read before Serializable: ");
+		System.out.println("username: " + user.getUsername());
+		System.err.println("password: " + user.getPasswd());
+
+		try {
+			ObjectOutputStream os = new ObjectOutputStream(
+					new FileOutputStream("C:/user.txt"));
+			os.writeObject(user); // 将User对象写进文件
+			os.flush();
+			os.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			ObjectInputStream is = new ObjectInputStream(new FileInputStream(
+					"C:/user.txt"));
+			user = (User2) is.readObject(); // 从流中读取User的数据
+			is.close();
+
+			System.out.println("\nread after Serializable: ");
+			System.out.println("username: " + user.getUsername());
+			System.err.println("password: " + user.getPasswd());
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class User1 implements Serializable {
+	private static final long serialVersionUID = 8294180014912103005L;
+
+	private String username;
+	private transient String passwd;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+}
